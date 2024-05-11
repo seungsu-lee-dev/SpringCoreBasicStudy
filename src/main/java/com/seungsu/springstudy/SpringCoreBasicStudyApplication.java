3554faa02cf7376bc26e1134166a6045b2150b63
seungsu-lee-dev <seungsu.lee.dev@gmail.com>
2024-05-11 21:28:42 +0900

    chore: 프로젝트 init

---
+++ src/main/java/com/seungsu/springstudy/SpringCoreBasicStudyApplication.java
@@ -0,0 +1,13 @@
+package com.seungsu.springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreBasicStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreBasicStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seungsu/springstudy/SpringCoreBasicStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungsu.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
